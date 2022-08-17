b22f2064e5c3f9efb49d60e197eb875e8fab6d80
marlusmarcos <marlusufrn@gmail.com>
2022-08-17 19:48:20 -0300

    usando app.use para verificar auth

--- express/renderHTML/index.js
+++ express/renderHTML/index.js
@@ -8,6 +8,19 @@ const path = require ('path')
 
 const basePath = path.join (__dirname, "arquivosHTML");
 
+const authCheck = function (req, res, next) {
+    req.authStatus = true;
+
+    if (req.authStatus) {
+        console.log ("está logado");
+        next();
+    } else {
+        console.log ("não está logado");
+        next();
+    } 
+}
+
+app.use(authCheck);
 
 app.get ("/", (req, res) => {
     res.sendFile (`${basePath}/index.html`);
