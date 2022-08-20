91fca42ddd797a08c8edc448fdbd29913d654b2a
marlusmarcos <marlusufrn@gmail.com>
2022-08-20 15:09:48 -0300

    post com sequelize

--- express/sequelize/index.js
+++ express/sequelize/index.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const exphbs = require ('express-handlebars');
 const conn = require ('./db/conn');
+const User = require('./models/User');
 const port = 3000;
 const app = express ();
 const user = require ('./models/User');
@@ -19,6 +20,27 @@ app.use (
 )
 
 app.use (express.json())
+
+app.get ('/users/create', (req, res) => {
+    res.render ('adduser');
+})
+
+app.post ('/users/create', async (req,res) => {
+    const name = req.body.name;
+    const ocuppation = req.body.ocuppation;
+    let newsLatter = req.body.newsLatter;
+    if (newsLatter === 'on') {
+        newsLatter = true 
+    } else {
+        newsLatter = false;
+       }
+          await User.create ({name,ocuppation,newsLatter});
+    res.redirect('/');
+})
+
+app.get ('/', (req,res) => {
+    res.render('home');
+}) 
 
 conn.sync().then (() => {
     app.listen (3000, (err) => {
